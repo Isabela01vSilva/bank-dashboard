ecc670c4fd81f21e304949297a17eccf460d04e1
Isabela01vSilva <isabela.estudosv067@gmail.com>
2026-05-25 12:25:32 -0300

    feat: Campos para filtrar

--- bank-module/src/app/features/page-transition/page-transition.ts
+++ bank-module/src/app/features/page-transition/page-transition.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Header } from "../../layout/header/header";
-import { LucideAngularModule, Plus  } from "lucide-angular";
+import { LucideAngularModule, Plus, Filter, Upload, Search  } from "lucide-angular";
 
 @Component({
   selector: 'app-page-transition',
@@ -9,4 +9,7 @@ import { LucideAngularModule, Plus  } from "lucide-angular";
 })
 export class PageTransition {
   readonly Plus = Plus;
+  readonly Filter = Filter;
+  readonly Upload = Upload;
+  readonly Search = Search;
 }
