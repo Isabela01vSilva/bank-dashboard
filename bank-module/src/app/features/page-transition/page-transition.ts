072a48ae15186026b1b2a10649f69dd588eae3ed
Isabela01vSilva <isabela.estudosv067@gmail.com>
2026-05-25 11:40:58 -0300

    feat: adicionar cabeçalho e botão de nova transação ao componente de transição de página

--- bank-module/src/app/features/page-transition/page-transition.ts
+++ bank-module/src/app/features/page-transition/page-transition.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from "../../layout/header/header";
+import { LucideAngularModule, Plus  } from "lucide-angular";
 
 @Component({
   selector: 'app-page-transition',
-  imports: [],
+  imports: [Header, LucideAngularModule],
   templateUrl: './page-transition.html',
 })
-export class PageTransition {}
+export class PageTransition {
+  readonly Plus = Plus;
+}
