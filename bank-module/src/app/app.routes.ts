7c1cccef0a51596533cdc79f0567d5bd13fae430
isabela01vSilva <isabela.estudosv067@gmail.com>
2026-07-17 18:35:57 -0300

    feat: page consulta de conta

--- bank-module/src/app/app.routes.ts
+++ bank-module/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
 import { PageTransition } from './features/page-transition/page-transition';
+import { AccountInquiryPage } from './features/account-inquiry-page/account-inquiry-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: Dashboard,
   },
+  { path: 'account',
+    component: AccountInquiryPage },
   {
     path: 'transactions',
     component: PageTransition
---
+++ bank-module/src/app/features/account-inquiry-page/account-inquiry-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-inquiry-page',
+  imports: [],
+  templateUrl: './account-inquiry-page.html',
+})
+export class AccountInquiryPage {}
